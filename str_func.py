20b2e410768814a5282673552834f6f633c55e3a
Artemii Dovzhenko <rockenny@ymail.com>
2023-03-25 01:24:20 +0300

    add str_capitalize add docstring str_upper

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_upper(string: str) -> str:
+    """Not so good docstring"""
     return string.upper()
+
+def str_capitalize(string: str) -> str:
+    """Make the first character have upper case and the rest lower case."""
+    return string.capitalize()
